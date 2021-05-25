19f7648c86322dfa04f80f3a8936c000511b7336
ignacio chiaradia <ignachy01@gmail.com>
2021-05-25 20:54:00 -0300

    main menu server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,55 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             if (args.isEmpty() || args[0] == "server") {
-                //TODO acá va el server
+                handleServer()
+                //handleClient()
             } else {
                 //TODO acá va el cliente
+            }
+        }
+
+        private fun handleServer() {
+            //inicia servidor
+            val server = Server(3000)
+            thread{server.run()}
+
+            //espera por comandos (?)
+            while (true){
+                //lee user input
+                when(readLine()){
+                    //muestra los clientes conectados
+                    "clients"->{
+                        val clients = server.getOnlineClients()
+
+                        if(clients.isEmpty()){
+                            println("No clients")
+                        }else{
+                            println("Clients (${clients.size}):")
+                            clients.forEach { println(it) }
+                        }
+                    }
+                    "stop"->{
+                        if(!server.stopped) {
+                            server.stop()
+                        }else{
+                            println("Server already stopped")
+                        }
+                    }
+                    "start"->{
+                        if(server.stopped){
+                            thread{server.run()}
+                        }else{
+                            println("Server already running")
+                        }
+
+                    }
+                    "info"->{
+                        val threadSet: Set<Thread> = Thread.getAllStackTraces().keys
+                        println("Threads (${threadSet.size}):")
+                        threadSet.forEach{println(it)}
+                    }
+                    else->println("Not a valid command")
+                }
             }
         }
     }
